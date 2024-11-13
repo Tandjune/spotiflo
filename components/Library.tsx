65e2a3989f543c6018c77d9642adedf1e7b4db24
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-13 14:56:49 +0100

    Upload Modal and functionality added

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,24 @@
-"use client"
+"use client";
 
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
 const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
 
     const onClick = () => {
-        //handle uplaod later
+        if (!user) {
+            return authModal.onOpen();
+        }
+        //TODO: Check for subscription
+
+        return uploadModal.onOpen()
     }
 
     return (
