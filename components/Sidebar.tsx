6dff3265c91b440959633517b4de785aa75e56b6
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-14 19:12:40 +0100

    Library comp. added in Sidebar comp.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,16 +5,19 @@ import { useMemo } from "react"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
 
+import { Song } from "@/types"
 import Box from "./Box"
 import SidebarItem from "./SideBarItem"
 import Library from "./Library"
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[];
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-    children
+    children,
+    songs
 }) => {
     const pathname = usePathname()
     const routes = useMemo(() => [
@@ -68,7 +71,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full ">
-                    <Library />
+                    <Library songs={songs} />
                 </Box>
 
             </div>
