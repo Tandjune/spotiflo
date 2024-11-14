ea2813671695b2bbab92034d34858f7ebdc33188
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-14 18:53:15 +0100

    getSongsByUserId action added

---
+++ actions/getSongsByUserId.ts
@@ -0,0 +1,29 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getSongsById = async (): Promise<Song[]> => {
+  const cookieStore = await cookies();
+  const supabase = createServerComponentClient({ cookies: cookies });
+
+  const { data: sessionData, error: sessionError } =
+    await supabase.auth.getUser();
+
+  if (sessionError) {
+    console.log(sessionError.message);
+  }
+
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .eq("user_id", sessionData.user?.id)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return (data as any) || [];
+};
+
+export default getSongsById;
