c60f570b7e740ddc63c5dfad4f4392ee2a287d21
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-16 04:15:52 +0100

    play songs in Library page

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,6 +3,7 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
+import useOnplay from "@/hooks/useOnPlay";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
@@ -19,6 +20,8 @@ const Library: React.FC<LibraryProps> = ({
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+
+    const onPlay = useOnplay(songs);
 
     const onClick = () => {
         if (!user) {
@@ -65,7 +68,7 @@ const Library: React.FC<LibraryProps> = ({
             <div className="flex flex-col gap-y-2 mt-4 px-3">
                 {songs.map((item) => (
                     <MediaItem
-                        onClick={() => { }}
+                        onClick={(id: string) => onPlay(id)}
                         key={item.id}
                         data={item}
                     />
