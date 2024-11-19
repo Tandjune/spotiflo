f7ac8d711afac0d58648945ccb7e9cbdf8a4e9b2
Florent Tandjune Tamoyem <tandjunetamoyem@gmail.com>
2024-11-19 12:10:54 +0100

    Loader changed

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -1,16 +1,16 @@
 "use client"
-import { ColorRing } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
 
 import Box from "@/components/Box";
 
 const Loading = () => {
     return (
         <Box className="h-full flex items-center justify-center">
-            <ColorRing
+            <Oval
                 visible={true}
                 height="80"
                 width="80"
-                colors={['#22c55e', '#22c55e', '#22c55e', '#22c55e', '#22c55e']}
+                color="#22c55e"
             />
         </Box>
     );
